2e9a5962feae8eda1177ea8af26d84c6002030ce
warikjames <warikjames@users.noreply.github.com>
2021-08-29 10:52:21 +1200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ io.on('connection', async (socket) => {
   
   const userId = await fetchUserId(socket);
   socket.join(userId);
-  socket.to(userId).emit('hi');
+  socket.to(userId).emit('chat message', 'hi'+ userId);
   
   //socket.broadcast.emit("connection", "Someone connected to the Chat");
   
