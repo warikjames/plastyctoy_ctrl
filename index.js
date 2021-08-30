e2782f40316461ac206f3136e9f7207f23a35c1e
warikjames <warikjames@users.noreply.github.com>
2021-08-31 08:42:36 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require("socket.io")(httpServer, {cors: {
-origin: "http://localhost:8888", // or "*"
-methods: ["GET", "POST"]}});
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
