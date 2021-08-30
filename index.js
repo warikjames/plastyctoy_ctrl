d7c2f1bc347acf5e6774436ac16208cf45e9bbb9
warikjames <warikjames@users.noreply.github.com>
2021-08-31 10:00:37 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const app = require('express')();
 var cors = require('cors');
 const http = require('http').Server(app, {
-  cors: {
-    origin: '*',
-  }
+  handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
@@ -19,6 +25,9 @@ app.get('/', cors(corsOptions), (req, res, next) => {
 });
 
 io.on('connection', (socket) => {
+  
+  console.log("Connected!");
+  
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
