a17e05ea97ddb969ec725e5780f45c80e7cf7363
warikjames <warikjames@users.noreply.github.com>
2021-08-31 08:57:19 +1200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/', (req, res, next) => {
+var corsOptions = {
+  origin: 'http://localhost:8888',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', cors(corsOptions), function (req, res, next) => {
   res.sendFile(__dirname + '/index.html');
   res.json({msg: 'This is CORS-enabled for all origins!'});
 });
