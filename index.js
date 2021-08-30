01bd5d9a27ab812211739187b17b9e47804b5b4b
warikjames <warikjames@users.noreply.github.com>
2021-08-31 09:48:41 +1200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: 'http://localhost:8888',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
