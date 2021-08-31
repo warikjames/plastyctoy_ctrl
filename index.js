40f52abee8423d126f55b83d25ca667895cb7273
warikjames <warikjames@users.noreply.github.com>
2021-08-31 14:55:02 +1200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,33 @@ app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const botName = 'Bot';
+
 io.on('connection', (socket) => {
+  
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  
+  
     
   socket.on('message', msg => {
     io.emit('message', msg);
