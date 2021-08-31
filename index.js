4c4a28447c496d2cde9aac7ae756ec04a3a07804
warikjames <warikjames@users.noreply.github.com>
2021-08-31 12:06:33 +1200

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.get('/', (req, res, next) => {
 });
 
 io.on('connection', (socket) => {
-  
-  window.addEventListener('load', (event) => {
-    io.emit('message', 'page is fully loaded');
-  });
     
   socket.on('message', msg => {
     io.emit('message', msg);
