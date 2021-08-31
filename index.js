53cb5c75fb20937512bb388cd34a26b57e3503c4
warikjames <warikjames@users.noreply.github.com>
2021-09-01 11:48:26 +1200

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ function getCurrentUser(id){
   return users.find(user => user.id === id);
 }
   
-function userLeave(id) {
-  const index = users.findIndex(user => user.id === id);
+// function userLeave(id) {
+//   const index = users.findIndex(user => user.id === id);
 
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
-  }
-}
+//   if (index !== -1) {
+//     return users.splice(index, 1)[0];
+//   }
+// }
 
 const botName = "ADMIN";
 
@@ -62,22 +62,22 @@ io.on('connection', (socket) => {
     io.to(user.room).emit('message', formatMessage(user.username, msg));
   });
   
-   socket.on('disconnect', () => {
-    const user = userLeave(socket.id);
+//    socket.on('disconnect', () => {
+//     const user = userLeave(socket.id);
 
-    if (user) {
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
+//     if (user) {
+//       io.to(user.room).emit(
+//         'message',
+//         formatMessage(botName, `${user.username} has left the chat`)
+//       );
 
-      // Send users and room info
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      });
-    }
-  });
+//       // Send users and room info
+//       io.to(user.room).emit('roomUsers', {
+//         room: user.room,
+//         users: getRoomUsers(user.room)
+//       });
+//     }
+//   });
   
 });
 
