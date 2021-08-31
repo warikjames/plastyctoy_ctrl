dfc290d6c1a4a9cb34050262058afecfd55137bc
warikjames <warikjames@users.noreply.github.com>
2021-08-31 15:57:40 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const app = require('express')();
 var cors = require('cors');
 const http = require('http').Server(app);
-const formatMessage = require('./utils/messages');
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers
-} = require('./utils/users');
+
 const io = require("socket.io")(http, {cors: {
 origin: "http://localhost:8888", // or "*"
 methods: ["GET", "POST"]}});
@@ -18,13 +12,7 @@ app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-const botName = 'ChatCord Bot';
-
 io.on('connection', (socket) => {
-  
-  socket.on('message', msg => {
-    io.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
-  });
   
   socket.on('message', msg => {
     io.emit('message', msg);
