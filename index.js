ea780d8528836c8451a6e31622965325e6827ac6
warikjames <warikjames@users.noreply.github.com>
2021-09-01 09:49:05 +1200

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ io.on('connection', (socket) => {
  socket.on('joinRoom', ({username, room}) => {
 //    const user = userJoin(socket.id, username, room);
    
+   socket.emit('message', formatMessage(username,room));
+   
 //    socket.join(user.room);
  });
   
